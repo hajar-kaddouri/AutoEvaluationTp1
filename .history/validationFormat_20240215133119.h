31fa7f90dae79f4538c22649c162bf9dd9adbb60
hajar-kaddouri <kad.hajar97@hotmail.com>
2024-02-15 14:17:51 -0500

    verification fonctions

---
+++ .history/validationFormat_20240215133119.h
@@ -0,0 +1,13 @@
+#ifndef VALIDATIONFORMAT_H
+#define VALIDATIONFORMAT_H
+
+#include <string>
+#include <istream>
+
+namespace util {
+    bool validePointCardinal(const std::string& p_cardinalite);
+    bool valideLigneCVSBorneStationnement(const std::string& p_ligne);
+    bool valideFichierBornesStationnement(std::istream& p_fluxBornesStationnement);
+}
+
+#endif // VALIDATIONFORMAT_H
